cc4939b1a66f4b46b4d73bead5faf17ff6dba422
Prasanna2002004 <kotkarprasanna96@gmail.com>
2024-07-12 12:18:35 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,115 @@
+import random as rnd
+import math
+from tkinter import *
+from tkinter import messagebox
+from word import word_list
+
+TIMER = 5
+number_of_words = 10
+words = ""
+user_total = ""
+word_total = ""
+
+
+def generate_words():
+    global number_of_words
+    random_word = ""
+    for i in range(number_of_words):
+        random_word = random_word + " " + rnd.choice(word_list)
+
+    return random_word.strip()
+
+
+def start():
+    global words
+    start_button.config(state="disabled")
+    words = generate_words()
+    canvas.itemconfig(test_text, text=words)
+    user_input.config(state="normal")
+    user_input.focus()
+    count_down(TIMER)
+
+
+def count_down(time_left):
+    time_sec = time_left % 60
+    time_min = math.floor(time_left / 60)
+
+    if time_sec < 10:
+        timer_text.config(text=f"Timer: {time_min}:0{time_sec}")
+    else:
+        timer_text.config(text=f"Timer: {time_min}:{time_sec}")
+
+    if time_sec == 0 and time_min == 0:
+        check(time_left)
+        start_button.config(state="normal")
+        user_input.delete(0, END)
+        user_input.config(state="disabled")
+        canvas.itemconfig(test_text, text="Click start to start the test.")
+
+    if time_left > 0:
+        window.after(100, check, time_left)
+        window.after(1000, count_down, time_left - 1)
+
+
+def check(rtime):
+    global words, user_total, word_total
+
+    user_text = user_input.get()
+    if len(words) == len(user_text):
+        user_total = user_total + " " + user_text
+        word_total = word_total + " " + words
+        words = generate_words()
+        user_input.delete(0, END)
+        canvas.itemconfig(test_text, text=words)
+
+    if rtime == 0:
+        user_total = user_total + " " + user_input.get()
+        word_total = word_total + " " + words[0:len(user_input.get())]
+        speed = len(user_total.strip().split(" ")) * 6
+        accuracy = get_accuracy(user_total.strip(), word_total.strip())
+        messagebox.showinfo(title="Result",
+                            message=f"Your typing results are:\nSpeed: {speed} WPM\nAccuracy: {accuracy}%")
+        print(user_text)
+        print(user_total.strip().split(" "))
+
+
+def get_accuracy(user, word):
+    counter = 0
+
+    for i in range(len(user)):
+        if user[i] == word[i]:
+            counter += 1
+
+    try:
+        accuracy = (counter / len(user)) * 100
+    except ZeroDivisionError:
+        return 0
+    else:
+        return f"{accuracy: .2f}"
+
+
+window = Tk()
+window.minsize(700, 500)
+window.title("Typing Tester")
+window.config(pady=100, padx=100)
+
+timer_text = Label(text="Timer: 00:00", font=("Arial", 24, "bold"), fg="red")
+timer_text.grid(column=2, row=0)
+
+canvas = Canvas(height=100, width=700)
+canvas.grid(column=2, row=1, columnspan=2)
+canvas.config(bg="black")
+test_text = canvas.create_text(350, 50,
+                               width=600,
+                               text="Click start to start the test.",
+                               fill="white",
+                               font=("Arial", 15, "bold"))  # width, height
+
+user_input = Entry(width=63, font=("Helvetica", 15))
+user_input.config(state="disabled")
+user_input.grid(column=2, row=2, columnspan=2)
+
+start_button = Button(text="Start the test", bg="green", font=("Arial", 15, "bold"), command=start)
+start_button.grid(column=2, row=3)
+
+window.mainloop()
---
+++ word.py
@@ -0,0 +1,11 @@
+word_list = [
+    "cat", "dog", "bat", "hat", "run", "fun", "cup", "hot", "ice", "red",
+    "car", "bus", "pen", "top", "egg", "box", "key", "zip", "map", "jam",
+    "ball", "bell", "book", "duck", "fish", "lamp", "moon", "nest", "star", "duck",
+    "cake", "flag", "frog", "king", "leaf", "milk", "pear", "rose", "sock", "song",
+    "bear", "bird", "door", "fork", "girl", "leaf", "lion", "meat", "nose", "rain",
+    "ring", "seed", "ship", "shoe", "skin", "snow", "toad", "toys", "worm", "yard",
+    "bank", "bird", "bone", "desk", "door", "dust", "face", "farm", "gate", "goat",
+    "glove", "grass", "house", "juice", "knife", "light", "mouse", "plant", "radio", "river",
+    "shelf", "shoe", "smile", "snake", "table", "tiger", "water", "wheel", "woman", "zebra"
+]
